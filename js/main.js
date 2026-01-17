85c7dd0c4d09d33ee0d45f580de638cb152683a3
erynaa <misyaeryna@gmail.com>
2026-01-17 23:57:04 +0800

    create booking form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 // Menu
-let menu = document.querySelector('.menu-icon');
+// let menu = document.querySelector('.menu-icon');
 
 // Animate Menu
 // menu.onclick = () => {
 //     menu.classList.toggle("move");
 // }
+
+const form = document.getElementById("bookingForm")
+
+if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const phone = document.getElementById("phone").value;
+        const car = document.getElementById("car").value;
+        const pickup = document.getElementById("pickup").value;
+        const returnDate = document.getElementById("return").value;
+
+        const message = `
+Khairul Car Rental | Tempahan Sewa Kereta
+
+Nama: ${name}
+No Telefon: ${phone}
+Kenderaan: ${car}
+Tarikh Ambil: ${pickup}
+Tarikh Pulang: ${returnDate}
+
+Terima kasih.
+        `;
+
+        const whatsappNumber = "601111976971";
+        const whatsappURL = "https://wa.me/" + whatsappNumber + "?text=" + encodeURIComponent(message);
+
+        window.open(whatsappURL, "_blank")
+    });
+}
