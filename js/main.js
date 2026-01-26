7138a2dd618d2ea5eda188d110d7876735848c29
erynaa <misyaeryna@gmail.com>
2026-01-26 21:25:30 +0800

    update css

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@
 //     menu.classList.toggle("move");
 // }
 
+// ==============================
+// BOOKING FORM
+// ==============================
 const form = document.getElementById("bookingForm")
 
 if (form) {
@@ -44,5 +47,33 @@ Kami akan menghubungi anda untuk pengesahan tempahan secepat mungkin.
         const whatsappURL = "https://wa.me/" + whatsappNumber + "?text=" + encodeURIComponent(message);
 
         window.open(whatsappURL, "_blank")
+    });
+}
+
+
+// ==============================
+// CAR FILTER
+// ==============================
+
+const filters = document.querySelectorAll(".filter-menu li");
+const cars = document.querySelectorAll(".rental-box");
+
+if (filters.length && cars.length) {
+    filters.forEach(filter => {
+        filter.addEventListener("click", () => {
+            // Active state
+            filters.forEach(f => f.classList.remove("active"));
+            filter.classList.add("active");
+
+            const type = filter.dataset.filter;
+
+            cars.forEach(car => {
+                if (type === "all" || car.dataset.category.toLowerCase() === type.toLowerCase()){
+                    car.style.display = "block";
+                } else {
+                    car.style.display = "none";
+                }
+            });
+        });
     });
 }
